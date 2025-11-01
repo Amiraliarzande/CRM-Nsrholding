86d01e0698d665cfff0b250d00ff093c5946edb7
Amiraliarzande <amiraliarzande8@gmail.com>
2025-11-01 19:12:53 +0330

    Persian response on permissions and setting MIT License for the project

--- core/app/accounts/api/v1/permissions.py
+++ core/app/accounts/api/v1/permissions.py
@@ -1,16 +1,22 @@
-from rest_framework import permissions
+from rest_framework import permissions, exceptions
 from app.accounts.models import UserType
 
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and 
-            request.user.type in [UserType.SUPERUSER]
-        )
+        if not request.user.is_authenticated:
+            raise exceptions.AuthenticationFailed(detail="ابتدا وارد حساب کاربری خود شوید.")
+
+        if request.user.type not in [UserType.SUPERUSER]:
+            raise exceptions.PermissionDenied(detail="اجازه دسترسی ندارید. فقط ادمین اصلی مجاز است.")
+
+        return True
 
 class IsModirOrSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and 
-            request.user.type in [UserType.SUPERUSER , UserType.MODIR_AMEL]
-        )
+        if not request.user.is_authenticated:
+            raise exceptions.AuthenticationFailed(detail="ابتدا وارد حساب کاربری خود شوید.")
+
+        if request.user.type not in [UserType.SUPERUSER , UserType.MODIR_AMEL]:
+            raise exceptions.PermissionDenied(detail="اجازه دسترسی ندارید. فقط ادمین اصلی و مدیرعامل مجاز است.")
+
+        return True
